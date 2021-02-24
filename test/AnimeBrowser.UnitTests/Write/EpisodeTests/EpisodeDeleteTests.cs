a5fb760d2f1bb885ce8e270478e5d5cada44758b
szilajka <szilajka1@gmail.com>
2021-02-24 13:42:24 +0100

    Add delete episode tests.

---
+++ test/AnimeBrowser.UnitTests/Write/EpisodeTests/EpisodeDeleteTests.cs
@@ -0,0 +1,173 @@
+﻿using AnimeBrowser.BL.Interfaces.Write;
+using AnimeBrowser.BL.Services.Write;
+using AnimeBrowser.Common.Exceptions;
+using AnimeBrowser.Common.Models.Enums;
+using AnimeBrowser.Data.Entities;
+using AnimeBrowser.Data.Interfaces.Read;
+using AnimeBrowser.Data.Interfaces.Write;
+using AnimeBrowser.UnitTests.Helpers;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeBrowser.UnitTests.Write.EpisodeTests
+{
+    [TestClass]
+    public class EpisodeDeleteTests : TestBase
+    {
+        private IList<Episode> allEpisodes;
+        private IList<Season> allSeasons;
+        private IList<AnimeInfo> allAnimeInfos;
+
+        [TestInitialize]
+        public void InitDb()
+        {
+            allAnimeInfos = new List<AnimeInfo> {
+                new AnimeInfo { Id = 1, Title = "JoJo's Bizarre Adventure", Description = string.Empty, IsNsfw = false },
+                new AnimeInfo { Id = 2, Title = "Kuroku no Basketball", Description = string.Empty, IsNsfw = false }
+            };
+            allSeasons = new List<Season>
+            {
+                 new Season{Id = 1, SeasonNumber = 1, Title = "Phantom Blood", Description = "In this season we know the story of Jonathan, Dio and Speedwagon, then Joseph and the Pillarmen's story",
+                    StartDate = new DateTime(2012, 1, 1, 0 ,0 ,0, DateTimeKind.Utc), EndDate = new DateTime(2012, 3, 5, 0 ,0 ,0, DateTimeKind.Utc),
+                    AirStatus = (int)AirStatusEnum.Aired, NumberOfEpisodes = 24, AnimeInfoId = 1,
+                    CoverCarousel = Encoding.UTF8.GetBytes("JoJoCarousel"), Cover = Encoding.UTF8.GetBytes("JoJoCover"),
+                },
+                new Season{Id = 2, SeasonNumber = 1, Title = "Stardust Crusaders", Description = "In this season we know the story of old Joseph and young Jotaro Kujo's story while they trying to get into Egypt.",
+                    StartDate = new DateTime(2014, 3, 1, 0 ,0 ,0, DateTimeKind.Utc), EndDate = new DateTime(2014, 7, 10, 0 ,0 ,0, DateTimeKind.Utc),
+                    AirStatus = (int)AirStatusEnum.Aired, NumberOfEpisodes = 24, AnimeInfoId = 1,
+                    CoverCarousel = Encoding.UTF8.GetBytes("JoJoCarousel"), Cover = Encoding.UTF8.GetBytes("JoJoCover"),
+                },
+                new Season{Id = 5401, SeasonNumber = 1, Title = "Stardust Crusaders", Description = "In this season we know the story of old Joseph and young Jotaro Kujo's story while they trying to get into Egypt.",
+                    StartDate = new DateTime(2014, 3, 1, 0 ,0 ,0, DateTimeKind.Utc), EndDate = new DateTime(2014, 7, 10, 0 ,0 ,0, DateTimeKind.Utc),
+                    AirStatus = (int)AirStatusEnum.Aired, NumberOfEpisodes = 24, AnimeInfoId = 412,
+                    CoverCarousel = Encoding.UTF8.GetBytes("JoJoCarousel"), Cover = Encoding.UTF8.GetBytes("JoJoCover"),
+                },
+            };
+            allEpisodes = new List<Episode> {
+                new Episode { Id = 1, EpisodeNumber = 1, AirStatus = (int)AirStatusEnum.Aired, Title = "Prologue", Description = "This episode tells the backstory of Jonathan and Dio and their fights",
+                    AirDate =  new DateTime(2012, 1, 1, 0, 0, 0, DateTimeKind.Utc), Cover = Encoding.UTF8.GetBytes("S1Ep1Cover"), SeasonId = 1, AnimeInfoId = 1},
+                new Episode { Id = 2, EpisodeNumber = 2, AirStatus = (int)AirStatusEnum.Aired, Title = "Beginning of something new", Description = "More fighting for the family.",
+                    AirDate =  new DateTime(2012, 1, 8, 0, 0, 0, DateTimeKind.Utc), Cover = Encoding.UTF8.GetBytes("S1Ep2Cover"), SeasonId = 1, AnimeInfoId = 1},
+                new Episode { Id = 3, EpisodeNumber = 1, AirStatus = (int)AirStatusEnum.Aired, Title = "Family relations", Description = "Jotaro is in prison and we will know who is Jotaro and the old man.",
+                    AirDate =  new DateTime(2014, 3, 1, 0, 0, 0, DateTimeKind.Utc), Cover = Encoding.UTF8.GetBytes("S2Ep1Cover"), SeasonId = 2, AnimeInfoId = 1},
+                new Episode { Id = 4, EpisodeNumber = 2, AirStatus = (int)AirStatusEnum.Aired, Title = "S2 Episode 2", Description = "This episode tells the backstory of Jonathan and Dio and their fights",
+                    AirDate =  new DateTime(2014, 3, 8, 0, 0, 0, DateTimeKind.Utc), Cover = Encoding.UTF8.GetBytes("S1Ep1Cover"), SeasonId = 2, AnimeInfoId = 1},
+                new Episode { Id = 5, EpisodeNumber = 3, AirStatus = (int)AirStatusEnum.Aired, Title = "S2 Episode 3", Description = "More fighting for the family.",
+                    AirDate =  new DateTime(2014, 3, 15, 0, 0, 0, DateTimeKind.Utc), Cover = Encoding.UTF8.GetBytes("S1Ep2Cover"), SeasonId = 2, AnimeInfoId = 1},
+                new Episode { Id = 6, EpisodeNumber = 4, AirStatus = (int)AirStatusEnum.Aired, Title = "S2 Episode 4", Description = "Jotaro is in prison and we will know who is Jotaro and the old man.",
+                    AirDate =  new DateTime(2014, 3, 21, 0, 0, 0, DateTimeKind.Utc), Cover = Encoding.UTF8.GetBytes("S2Ep1Cover"), SeasonId = 2, AnimeInfoId = 1},
+                new Episode { Id = 7, EpisodeNumber = 5, AirStatus = (int)AirStatusEnum.Aired, Title = "S2 Episode 5", Description = "More fighting for the family.",
+                    AirDate =  new DateTime(2014, 3, 28, 0, 0, 0, DateTimeKind.Utc), Cover = Encoding.UTF8.GetBytes("S1Ep2Cover"), SeasonId = 2, AnimeInfoId = 1},
+                new Episode { Id = 8, EpisodeNumber = 6, AirStatus = (int)AirStatusEnum.Aired, Title = "S2 Episode 6", Description = "Jotaro is in prison and we will know who is Jotaro and the old man.",
+                    AirDate =  new DateTime(2014, 4, 5, 0, 0, 0, DateTimeKind.Utc), Cover = Encoding.UTF8.GetBytes("S2Ep1Cover"), SeasonId = 2, AnimeInfoId = 1}
+            };
+        }
+
+
+        [DataTestMethod,
+            DataRow(1), DataRow(2), DataRow(3), DataRow(4)]
+        public async Task DeleteEpisode_ShouldWork(long episodeId)
+        {
+            Episode foundEpisode = null;
+            var sp = SetupDI(services =>
+            {
+                var episodeReadRepo = new Mock<IEpisodeRead>();
+                var episodeWriteRepo = new Mock<IEpisodeWrite>();
+
+                episodeReadRepo.Setup(er => er.GetEpisodeById(It.IsAny<long>())).Callback<long>(eId => foundEpisode = allEpisodes.SingleOrDefault(e => e.Id == eId)).ReturnsAsync(() => foundEpisode);
+                episodeWriteRepo.Setup(ew => ew.DeleteEpisode(It.IsAny<Episode>())).Callback<Episode>(e => allEpisodes.Remove(e));
+
+                services.AddTransient(factory => episodeReadRepo.Object);
+                services.AddTransient(factory => episodeWriteRepo.Object);
+                services.AddTransient<IEpisodeDelete, EpisodeDeleteHandler>();
+            });
+
+            var beforeEpisodeCount = allEpisodes.Count;
+            var episodeDeleteHandler = sp.GetService<IEpisodeDelete>();
+            await episodeDeleteHandler.DeleteEpisode(episodeId);
+            foundEpisode.Should().NotBeNull();
+            allEpisodes.Should().NotContain(foundEpisode);
+            allEpisodes.Count.Should().Be(beforeEpisodeCount - 1);
+        }
+
+
+        [DataTestMethod,
+            DataRow(0), DataRow(-1), DataRow(-100), DataRow(-4040)]
+        public async Task DeleteEpisode_InvalidId_ThrowException(long episodeId)
+        {
+            var sp = SetupDI(services =>
+            {
+                var episodeReadRepo = new Mock<IEpisodeRead>();
+                var episodeWriteRepo = new Mock<IEpisodeWrite>();
+
+                services.AddTransient(factory => episodeReadRepo.Object);
+                services.AddTransient(factory => episodeWriteRepo.Object);
+                services.AddTransient<IEpisodeDelete, EpisodeDeleteHandler>();
+            });
+
+            var episodeDeleteHandler = sp.GetService<IEpisodeDelete>();
+            Func<Task> episodeDeleteFunc = async () => await episodeDeleteHandler.DeleteEpisode(episodeId);
+            await episodeDeleteFunc.Should().ThrowAsync<NotExistingIdException>();
+        }
+
+        [DataTestMethod,
+            DataRow(123), DataRow(9), DataRow(300)]
+        public async Task DeleteEpisode_NotExistingId_ThrowException(long episodeId)
+        {
+            Episode foundEpisode = null;
+            var sp = SetupDI(services =>
+            {
+                var episodeReadRepo = new Mock<IEpisodeRead>();
+                var episodeWriteRepo = new Mock<IEpisodeWrite>();
+
+                episodeReadRepo.Setup(er => er.GetEpisodeById(It.IsAny<long>())).Callback<long>(eId => foundEpisode = allEpisodes.SingleOrDefault(e => e.Id == eId)).ReturnsAsync(() => foundEpisode);
+
+                services.AddTransient(factory => episodeReadRepo.Object);
+                services.AddTransient(factory => episodeWriteRepo.Object);
+                services.AddTransient<IEpisodeDelete, EpisodeDeleteHandler>();
+            });
+
+            var episodeDeleteHandler = sp.GetService<IEpisodeDelete>();
+            Func<Task> episodeDeleteFunc = async () => await episodeDeleteHandler.DeleteEpisode(episodeId);
+            await episodeDeleteFunc.Should().ThrowAsync<NotFoundObjectException<Episode>>();
+        }
+
+        [TestMethod]
+        public async Task DeleteEpisode_ThrowException()
+        {
+            var sp = SetupDI(services =>
+            {
+                var episodeReadRepo = new Mock<IEpisodeRead>();
+                var episodeWriteRepo = new Mock<IEpisodeWrite>();
+
+                episodeReadRepo.Setup(er => er.GetEpisodeById(It.IsAny<long>())).ThrowsAsync(new InvalidOperationException());
+
+                services.AddTransient(factory => episodeReadRepo.Object);
+                services.AddTransient(factory => episodeWriteRepo.Object);
+                services.AddTransient<IEpisodeDelete, EpisodeDeleteHandler>();
+            });
+
+            var episodeDeleteHandler = sp.GetService<IEpisodeDelete>();
+            Func<Task> episodeDeleteFunc = async () => await episodeDeleteHandler.DeleteEpisode(1);
+            await episodeDeleteFunc.Should().ThrowAsync<InvalidOperationException>();
+        }
+
+        [TestCleanup]
+        public void CleanDb()
+        {
+            allEpisodes.Clear();
+            allSeasons.Clear();
+            allAnimeInfos.Clear();
+            allEpisodes = null;
+            allSeasons = null;
+            allAnimeInfos = null;
+        }
+    }
+}
