4b3d28a608ff223729a776c801b89003599497db
szilajka <szilajka1@gmail.com>
2021-03-11 13:56:04 +0100

    Add more season name editing tests.

--- test/AnimeBrowser.UnitTests/Write/SeasonNameTests/SeasonNameEditingTests.cs
+++ test/AnimeBrowser.UnitTests/Write/SeasonNameTests/SeasonNameEditingTests.cs
@@ -1,6 +1,8 @@
 ﻿using AnimeBrowser.BL.Interfaces.Write.SecondaryInterfaces;
 using AnimeBrowser.BL.Services.Write.SecondaryHandlers;
+using AnimeBrowser.Common.Exceptions;
 using AnimeBrowser.Common.Models.Enums;
+using AnimeBrowser.Common.Models.ErrorModels;
 using AnimeBrowser.Common.Models.RequestModels.SecondaryModels;
 using AnimeBrowser.Data.Converters.SecondaryConverters;
 using AnimeBrowser.Data.Entities;
@@ -35,11 +37,14 @@ namespace AnimeBrowser.UnitTests.Write.SeasonNameTests
             allRequestModels = new List<SeasonNameEditingRequestModel>();
 
             var ids = new long[] { 31, 31, 32, 32, 33,
-                34, 34, 35, 35, 41 };
+                34, 34, 35, 35, 41,
+                31, 32, 33 };
             var titles = new string[] { "ファントムブラッド", "Fantomu Buraddo", "スターダストクルセイダース", "Sutādasuto Kuruseidāsu", "僕のヒーローアカデミア Season 1",
-                "ドロヘドロ Season 1", "ドロヘドロ Kaiman", "Shingeki no Kyojin Season 4", "Shingeki no Kyojin Finale Season", "The Promised Neverland Season 2" };
+                "ドロヘドロ Season 1", "ドロヘドロ Kaiman", "Shingeki no Kyojin Season 4", "Shingeki no Kyojin Finale Season", "The Promised Neverland Season 2," +
+                "T", new string('T', 254), new string('T', 255) };
             var seasonIds = new long[] { 1, 1, 2, 2, 3,
-                10, 10, 20, 20, 22 };
+                10, 10, 20, 20, 22,
+                1, 2, 3 };
             for (var i = 0; i < titles.Length; i++)
             {
                 allRequestModels.Add(new SeasonNameEditingRequestModel(id: ids[i], title: titles[i], seasonId: seasonIds[i]));
@@ -99,7 +104,8 @@ namespace AnimeBrowser.UnitTests.Write.SeasonNameTests
                 new SeasonName { Id = 33, Title = "Boku no Heroes Season 1", SeasonId = 3 },
                 new SeasonName { Id = 34, Title = "Dorohedoro - Kaiman's story", SeasonId = 10 },
                 new SeasonName { Id = 35, Title = "Finale Season", SeasonId = 20 },
-                new SeasonName { Id = 41, Title = "The Second Season", SeasonId = 22 }
+                new SeasonName { Id = 41, Title = "The Second Season", SeasonId = 22 },
+                new SeasonName { Id = 42, Title = "JoJo Part 1-2 Anime", SeasonId = 1 }
             };
         }
 
@@ -112,6 +118,58 @@ namespace AnimeBrowser.UnitTests.Write.SeasonNameTests
             }
         }
 
+        private static IEnumerable<object[]> GetMismatchingIdData()
+        {
+            var ids = new long?[] { 5, 10, 5323, null };
+            for (var i = 0; i < ids.Length; i++)
+            {
+                var srm = allRequestModels[i];
+                yield return new object[] { ids[i], new SeasonNameEditingRequestModel(id: srm.Id, title: srm.Title, seasonId: srm.SeasonId) };
+            }
+        }
+
+        private static IEnumerable<object[]> GetNotExistingSeasonNameIdData()
+        {
+            var ids = new long[] { 90, 0, -1, 5323 };
+            for (var i = 0; i < ids.Length; i++)
+            {
+                var srm = allRequestModels[i];
+                yield return new object[] { ids[i], new SeasonNameEditingRequestModel(id: ids[i], title: srm.Title, seasonId: srm.SeasonId) };
+            }
+        }
+
+        private static IEnumerable<object[]> GetNotExistingSeasonIdData()
+        {
+            var seasonIds = new long[] { 90, 0, -1, 5323 };
+            for (var i = 0; i < seasonIds.Length; i++)
+            {
+                var srm = allRequestModels[i];
+                yield return new object[] { srm.Id, new SeasonNameEditingRequestModel(id: srm.Id, title: srm.Title, seasonId: seasonIds[i]) };
+            }
+        }
+
+        private static IEnumerable<object[]> GetInvalidTitleData()
+        {
+            var propertyName = nameof(SeasonNameEditingRequestModel.Title);
+            var errorCodes = new ErrorCodes[] { ErrorCodes.EmptyProperty, ErrorCodes.EmptyProperty, ErrorCodes.TooLongProperty, ErrorCodes.TooLongProperty };
+            var titles = new string[] { "", null, new string('T', 256), new string('T', 300) };
+            for (var i = 0; i < titles.Length; i++)
+            {
+                var srm = allRequestModels[i];
+                yield return new object[] { srm.Id, new SeasonNameEditingRequestModel(id: srm.Id, title: titles[i], seasonId: srm.SeasonId), errorCodes[i], propertyName };
+            }
+        }
+
+        private static IEnumerable<object[]> GetAlreadyExistingTitleData()
+        {
+            var titles = new string[] { "JoJo Part 1-2", "Phantom Blood", "Stardust Crusaders", "Season 1" };
+            var ids = new long[] { 42, 31, 32, 33 };
+            var seasonIds = new long[] { 1, 1, 2, 3 };
+            for (var i = 0; i < titles.Length; i++)
+            {
+                yield return new object[] { ids[i], new SeasonNameEditingRequestModel(id: ids[i], title: titles[i], seasonId: seasonIds[i]) };
+            }
+        }
 
 
         [DataTestMethod,
@@ -149,6 +207,176 @@ namespace AnimeBrowser.UnitTests.Write.SeasonNameTests
             updatedResponseModel.Should().BeEquivalentTo(responseModel);
             allSeasonNames.Should().ContainEquivalentOf(seasonName);
 
+        }
+
+        [DataTestMethod,
+            DynamicData(nameof(GetMismatchingIdData), DynamicDataSourceType.Method)]
+        public async Task EditSeasonName_MismatchingId_ThrowException(long id, SeasonNameEditingRequestModel requestModel)
+        {
+            var sp = SetupDI(services =>
+            {
+                var seasonReadRepo = new Mock<ISeasonRead>();
+                var seasonNameReadRepo = new Mock<ISeasonNameRead>();
+                var seasonNameWriteRepo = new Mock<ISeasonNameWrite>();
+                services.AddTransient(factory => seasonReadRepo.Object);
+                services.AddTransient(factory => seasonNameReadRepo.Object);
+                services.AddTransient(factory => seasonNameWriteRepo.Object);
+                services.AddTransient<ISeasonNameEditing, SeasonNameEditingHandler>();
+            });
+
+            var seasonNameEditingHandler = sp.GetService<ISeasonNameEditing>();
+            Func<Task> seasonNameEditingFunc = async () => await seasonNameEditingHandler!.EditSeasonName(id, requestModel);
+            await seasonNameEditingFunc.Should().ThrowAsync<MismatchingIdException>();
+        }
+
+        [TestMethod]
+        public async Task EditSeasonName_EmptyObject_ThrowException()
+        {
+            var sp = SetupDI(services =>
+            {
+                var seasonReadRepo = new Mock<ISeasonRead>();
+                var seasonNameReadRepo = new Mock<ISeasonNameRead>();
+                var seasonNameWriteRepo = new Mock<ISeasonNameWrite>();
+                services.AddTransient(factory => seasonReadRepo.Object);
+                services.AddTransient(factory => seasonNameReadRepo.Object);
+                services.AddTransient(factory => seasonNameWriteRepo.Object);
+                services.AddTransient<ISeasonNameEditing, SeasonNameEditingHandler>();
+            });
+
+            var seasonNameEditingHandler = sp.GetService<ISeasonNameEditing>();
+            Func<Task> seasonNameEditingFunc = async () => await seasonNameEditingHandler!.EditSeasonName(1, null);
+            await seasonNameEditingFunc.Should().ThrowAsync<MismatchingIdException>();
+        }
+
+        [DataTestMethod,
+            DynamicData(nameof(GetNotExistingSeasonNameIdData), DynamicDataSourceType.Method)]
+        public async Task EditSeasonName_NotExistingSeasonNameId_ThrowException(long id, SeasonNameEditingRequestModel requestModel)
+        {
+            SeasonName foundSeasonName = null;
+            var sp = SetupDI(services =>
+            {
+                var seasonReadRepo = new Mock<ISeasonRead>();
+                var seasonNameReadRepo = new Mock<ISeasonNameRead>();
+                var seasonNameWriteRepo = new Mock<ISeasonNameWrite>();
+
+                seasonNameReadRepo.Setup(snr => snr.GetSeasonNameById(It.IsAny<long>())).Callback<long>(snId => foundSeasonName = allSeasonNames.SingleOrDefault(sn => sn.Id == snId)).ReturnsAsync(() => foundSeasonName);
+
+                services.AddTransient(factory => seasonReadRepo.Object);
+                services.AddTransient(factory => seasonNameReadRepo.Object);
+                services.AddTransient(factory => seasonNameWriteRepo.Object);
+                services.AddTransient<ISeasonNameEditing, SeasonNameEditingHandler>();
+            });
+
+            var seasonNameEditingHandler = sp.GetService<ISeasonNameEditing>();
+            Func<Task> seasonNameEditingFunc = async () => await seasonNameEditingHandler!.EditSeasonName(id, requestModel);
+            await seasonNameEditingFunc.Should().ThrowAsync<NotFoundObjectException<SeasonName>>();
+        }
+
+        [DataTestMethod,
+            DynamicData(nameof(GetNotExistingSeasonIdData), DynamicDataSourceType.Method)]
+        public async Task EditSeasonName_NotExistingSeasonId_ThrowException(long id, SeasonNameEditingRequestModel requestModel)
+        {
+            SeasonName foundSeasonName = null;
+            Season foundSeason = null;
+            var sp = SetupDI(services =>
+            {
+                var seasonReadRepo = new Mock<ISeasonRead>();
+                var seasonNameReadRepo = new Mock<ISeasonNameRead>();
+                var seasonNameWriteRepo = new Mock<ISeasonNameWrite>();
+
+                seasonNameReadRepo.Setup(snr => snr.GetSeasonNameById(It.IsAny<long>())).Callback<long>(snId => foundSeasonName = allSeasonNames.SingleOrDefault(sn => sn.Id == snId)).ReturnsAsync(() => foundSeasonName);
+                seasonReadRepo.Setup(sr => sr.GetSeasonById(It.IsAny<long>())).Callback<long>(sId => foundSeason = allSeasons.SingleOrDefault(s => s.Id == sId)).ReturnsAsync(() => foundSeason);
+
+                services.AddTransient(factory => seasonReadRepo.Object);
+                services.AddTransient(factory => seasonNameReadRepo.Object);
+                services.AddTransient(factory => seasonNameWriteRepo.Object);
+                services.AddTransient<ISeasonNameEditing, SeasonNameEditingHandler>();
+            });
+
+            var seasonNameEditingHandler = sp.GetService<ISeasonNameEditing>();
+            Func<Task> seasonNameEditingFunc = async () => await seasonNameEditingHandler!.EditSeasonName(id, requestModel);
+            await seasonNameEditingFunc.Should().ThrowAsync<NotFoundObjectException<Season>>();
+        }
+
+        [DataTestMethod,
+            DynamicData(nameof(GetInvalidTitleData), DynamicDataSourceType.Method)]
+        public async Task EditSeasonName_InvalidTitle_ThrowException(long id, SeasonNameEditingRequestModel requestModel, ErrorCodes errorCode, string propertyName)
+        {
+            var errors = CreateErrorList(errorCode, propertyName);
+            SeasonName foundSeasonName = null;
+            Season foundSeason = null;
+            var sp = SetupDI(services =>
+            {
+                var seasonReadRepo = new Mock<ISeasonRead>();
+                var seasonNameReadRepo = new Mock<ISeasonNameRead>();
+                var seasonNameWriteRepo = new Mock<ISeasonNameWrite>();
+
+                seasonNameReadRepo.Setup(snr => snr.GetSeasonNameById(It.IsAny<long>())).Callback<long>(snId => foundSeasonName = allSeasonNames.SingleOrDefault(sn => sn.Id == snId)).ReturnsAsync(() => foundSeasonName);
+                seasonReadRepo.Setup(sr => sr.GetSeasonById(It.IsAny<long>())).Callback<long>(sId => foundSeason = allSeasons.SingleOrDefault(s => s.Id == sId)).ReturnsAsync(() => foundSeason);
+
+                services.AddTransient(factory => seasonReadRepo.Object);
+                services.AddTransient(factory => seasonNameReadRepo.Object);
+                services.AddTransient(factory => seasonNameWriteRepo.Object);
+                services.AddTransient<ISeasonNameEditing, SeasonNameEditingHandler>();
+            });
+
+            var seasonNameEditingHandler = sp.GetService<ISeasonNameEditing>();
+            Func<Task> seasonNameEditingFunc = async () => await seasonNameEditingHandler!.EditSeasonName(id, requestModel);
+            var valEx = await seasonNameEditingFunc.Should().ThrowAsync<ValidationException>();
+            valEx.And.Errors.Should().BeEquivalentTo(errors, options => options.Excluding(x => x.Description));
+        }
+
+        [DataTestMethod,
+            DynamicData(nameof(GetAlreadyExistingTitleData), DynamicDataSourceType.Method)]
+        public async Task EditSeasonName_AlreadyExistingTitle_ThrowException(long id, SeasonNameEditingRequestModel requestModel)
+        {
+            SeasonName foundSeasonName = null;
+            Season foundSeason = null;
+            bool isExistsWithSameTitle = false;
+            var sp = SetupDI(services =>
+            {
+                var seasonReadRepo = new Mock<ISeasonRead>();
+                var seasonNameReadRepo = new Mock<ISeasonNameRead>();
+                var seasonNameWriteRepo = new Mock<ISeasonNameWrite>();
+
+                seasonNameReadRepo.Setup(snr => snr.GetSeasonNameById(It.IsAny<long>())).Callback<long>(snId => foundSeasonName = allSeasonNames.SingleOrDefault(sn => sn.Id == snId)).ReturnsAsync(() => foundSeasonName);
+                seasonReadRepo.Setup(sr => sr.GetSeasonById(It.IsAny<long>())).Callback<long>(sId => foundSeason = allSeasons.SingleOrDefault(s => s.Id == sId)).ReturnsAsync(() => foundSeason);
+                seasonNameReadRepo.Setup(snr => snr.IsExistsWithSameTitle(It.IsAny<long>(), It.IsAny<string>(), It.IsAny<long>()))
+                    .Callback<long, string, long>((snId, snTitle, snSeasonId) => isExistsWithSameTitle = allSeasonNames.Any(sn => sn.Id != snId && sn.SeasonId == snSeasonId && sn.Title.Equals(snTitle, StringComparison.OrdinalIgnoreCase)))
+                    .Returns(() => isExistsWithSameTitle);
+
+                services.AddTransient(factory => seasonReadRepo.Object);
+                services.AddTransient(factory => seasonNameReadRepo.Object);
+                services.AddTransient(factory => seasonNameWriteRepo.Object);
+                services.AddTransient<ISeasonNameEditing, SeasonNameEditingHandler>();
+            });
+
+            var seasonNameEditingHandler = sp.GetService<ISeasonNameEditing>();
+            Func<Task> seasonNameEditingFunc = async () => await seasonNameEditingHandler!.EditSeasonName(id, requestModel);
+            await seasonNameEditingFunc.Should().ThrowAsync<AlreadyExistingObjectException<SeasonName>>();
+        }
+
+        [DataTestMethod,
+            DynamicData(nameof(GetBasicData), DynamicDataSourceType.Method)]
+        public async Task EditSeasonName_ThrowException(long id, SeasonNameEditingRequestModel requestModel)
+        {
+            var sp = SetupDI(services =>
+            {
+                var seasonReadRepo = new Mock<ISeasonRead>();
+                var seasonNameReadRepo = new Mock<ISeasonNameRead>();
+                var seasonNameWriteRepo = new Mock<ISeasonNameWrite>();
+
+                seasonNameReadRepo.Setup(snr => snr.GetSeasonNameById(It.IsAny<long>())).ThrowsAsync(new InvalidOperationException());
+
+                services.AddTransient(factory => seasonReadRepo.Object);
+                services.AddTransient(factory => seasonNameReadRepo.Object);
+                services.AddTransient(factory => seasonNameWriteRepo.Object);
+                services.AddTransient<ISeasonNameEditing, SeasonNameEditingHandler>();
+            });
+
+            var seasonNameEditingHandler = sp.GetService<ISeasonNameEditing>();
+            Func<Task> seasonNameEditingFunc = async () => await seasonNameEditingHandler!.EditSeasonName(id, requestModel);
+            await seasonNameEditingFunc.Should().ThrowAsync<InvalidOperationException>();
         }
 
 
