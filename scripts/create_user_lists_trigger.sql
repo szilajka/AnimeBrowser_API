96e878694c14a022bd44130deb48c686930a3f54
szilajka <szilajka1@gmail.com>
2021-03-18 12:20:49 +0100

    Fix create user list trigger, add update season ratings trigger.

--- scripts/create_user_lists_trigger.sql
+++ scripts/create_user_lists_trigger.sql
@@ -1,6 +1,6 @@
 --
--- TOC entry 235 (class 1255 OID 42445)
--- Name: create_user_list(); Type: FUNCTION; Schema: public; Owner: postgres
+-- TOC entry 236 (class 1255 OID 42445)
+-- Name: create_user_list(); Type: FUNCTION; Schema: public; Owner: ab_user
 --
 
 CREATE FUNCTION public.create_user_list() RETURNS trigger
@@ -13,6 +13,7 @@ BEGIN
         VALUES ("Watching", 20, TRUE, NEW."Id");
     INSERT INTO "public"."media_list" ("name", "list_type", "is_public", "user_id")
         VALUES ("Watched", 30, TRUE, NEW."Id");
+    RETURN NEW;
 END;
 $$;
 
@@ -20,9 +21,11 @@ $$;
 ALTER FUNCTION public.create_user_list() OWNER TO ab_user;
 
 
+
 --
--- TOC entry 3038 (class 2620 OID 42446)
+-- TOC entry 3039 (class 2620 OID 42451)
 -- Name: Users tg_create_user_list; Type: TRIGGER; Schema: identity; Owner: ab_user
 --
 
 CREATE TRIGGER tg_create_user_list AFTER INSERT ON identity."Users" FOR EACH ROW EXECUTE FUNCTION public.create_user_list();
+
