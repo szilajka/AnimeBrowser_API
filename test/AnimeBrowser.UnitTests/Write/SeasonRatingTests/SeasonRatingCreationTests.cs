c447a8ffd2f4ef3e76c15105c5d1b41d42e44951
szilajka <szilajka1@gmail.com>
2021-05-11 11:24:20 +0200

    Fix season rating tests.

--- test/AnimeBrowser.UnitTests/Write/SeasonRatingTests/SeasonRatingCreationTests.cs
+++ test/AnimeBrowser.UnitTests/Write/SeasonRatingTests/SeasonRatingCreationTests.cs
@@ -70,8 +70,8 @@ namespace AnimeBrowser.UnitTests.Write.SeasonRatingTests
             };
 
             allAnimeInfos = new List<AnimeInfo> {
-                new AnimeInfo { Id = 1, Title = "JoJo's Bizarre Adventure", Description = string.Empty, IsNsfw = false },
-                new AnimeInfo { Id = 2, Title = "Kuroku no Basketball", Description = string.Empty, IsNsfw = false }
+                new AnimeInfo { Id = 1, Title = "JoJo's Bizarre Adventure", Description = string.Empty, IsNsfw = false, IsActive = true },
+                new AnimeInfo { Id = 2, Title = "Kuroku no Basketball", Description = string.Empty, IsNsfw = false, IsActive = true }
             };
 
             allSeasons = new List<Season>
@@ -80,40 +80,45 @@ namespace AnimeBrowser.UnitTests.Write.SeasonRatingTests
                     StartDate = new DateTime(2012, 1, 1, 0 ,0 ,0, DateTimeKind.Utc), EndDate = new DateTime(2012, 3, 5, 0 ,0 ,0, DateTimeKind.Utc),
                     AirStatus = (int)AirStatuses.Aired, NumberOfEpisodes = 24, AnimeInfoId = 1,
                     CoverCarousel = Encoding.UTF8.GetBytes("JoJoCarousel"), Cover = Encoding.UTF8.GetBytes("JoJoCover"),
+                    IsAnimeInfoActive = true, IsActive = true
                 },
                 new Season{ Id = 2, SeasonNumber = 1, Title = "Stardust Crusaders", Description = "In this season we know the story of old Joseph and young Jotaro Kujo's story while they trying to get into Egypt.",
                     StartDate = new DateTime(2014, 3, 1, 0 ,0 ,0, DateTimeKind.Utc), EndDate = new DateTime(2014, 7, 10, 0 ,0 ,0, DateTimeKind.Utc),
                     AirStatus = (int)AirStatuses.Aired, NumberOfEpisodes = 24, AnimeInfoId = 1,
                     CoverCarousel = Encoding.UTF8.GetBytes("JoJoCarousel"), Cover = Encoding.UTF8.GetBytes("JoJoCover"),
+                    IsAnimeInfoActive = true, IsActive = true
                 },
                 new Season{ Id = 5401, SeasonNumber = 1, Title = "The Pillarmen's revenge", Description = "In this season the pillarmen are taking revenge for their death.",
                     StartDate = new DateTime(2014, 3, 1, 0 ,0 ,0, DateTimeKind.Utc), EndDate = new DateTime(2014, 7, 10, 0 ,0 ,0, DateTimeKind.Utc),
                     AirStatus = (int)AirStatuses.Aired, NumberOfEpisodes = 24, AnimeInfoId = 2,
                     CoverCarousel = Encoding.UTF8.GetBytes("JoJoCarousel"), Cover = Encoding.UTF8.GetBytes("JoJoCover"),
+                    IsAnimeInfoActive = true, IsActive = true
                 },
                 new Season{ Id = 5405, SeasonNumber = 1, Title = "Life is basketball", Description = "We know the MC, who wants to get his revenge for kicking her out of the basketball team by making a new team.",
                     StartDate = today.AddYears(-10).AddMonths(-3), EndDate = today.AddYears(-9),
                     AirStatus = (int)AirStatuses.Aired, NumberOfEpisodes = 24, AnimeInfoId = 2,
                     CoverCarousel = Encoding.UTF8.GetBytes("Basketball Carousel"), Cover = Encoding.UTF8.GetBytes("Basketball Cover"),
+                    IsAnimeInfoActive = true, IsActive = true
                 },
                   new Season{ Id = 6001, SeasonNumber = 1, Title = "Monochrome", Description = "Mc sees everything in monochrome. Due to his illness, demons attack him.",
                     StartDate = null, EndDate = null,
                     AirStatus = (int)AirStatuses.NotAired, NumberOfEpisodes = 10, AnimeInfoId = 2,
                     CoverCarousel = Encoding.UTF8.GetBytes("Basketball Carousel"), Cover = Encoding.UTF8.GetBytes("Basketball Cover"),
+                    IsAnimeInfoActive = true, IsActive = true
                 }
             };
 
             allSeasonRatings = new List<SeasonRating>
             {
-                new SeasonRating { Id = 1, Rating = 4, Message = "", SeasonId = 1, UserId = "15A6B54C-98D0-4396-90E7-C94761DBA977" },
-                new SeasonRating { Id = 2, Rating = 3, Message = "Not bad.", SeasonId = 2, UserId = "15A6B54C-98D0-4396-90E7-C94761DBA977" },
-                new SeasonRating { Id = 3, Rating = 5, Message = "Cool anime!", SeasonId = 5401, UserId = "15A6B54C-98D0-4396-90E7-C94761DBA977" },
-                new SeasonRating { Id = 4, Rating = 1, Message = "Very very bad....", SeasonId = 5405, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35" },
-                new SeasonRating { Id = 5, Rating = 2, Message = "", SeasonId = 5401, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35" },
-                new SeasonRating { Id = 6, Rating = 3, Message = "", SeasonId = 2, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35" },
-                new SeasonRating { Id = 7, Rating = 4, Message = "", SeasonId = 1, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35" },
-                new SeasonRating { Id = 8, Rating = 2, Message = "", SeasonId = 5401, UserId = "5879560D-65C5-4699-9449-86CC57EF3111" },
-                new SeasonRating { Id = 9, Rating = 5, Message = "", SeasonId = 5405, UserId = "5879560D-65C5-4699-9449-86CC57EF3111" },
+                new SeasonRating { Id = 1, Rating = 4, Message = "", SeasonId = 1, UserId = "15A6B54C-98D0-4396-90E7-C94761DBA977", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 2, Rating = 3, Message = "Not bad.", SeasonId = 2, UserId = "15A6B54C-98D0-4396-90E7-C94761DBA977", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 3, Rating = 5, Message = "Cool anime!", SeasonId = 5401, UserId = "15A6B54C-98D0-4396-90E7-C94761DBA977", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 4, Rating = 1, Message = "Very very bad....", SeasonId = 5405, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 5, Rating = 2, Message = "", SeasonId = 5401, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 6, Rating = 3, Message = "", SeasonId = 2, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 7, Rating = 4, Message = "", SeasonId = 1, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 8, Rating = 2, Message = "", SeasonId = 5401, UserId = "5879560D-65C5-4699-9449-86CC57EF3111", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 9, Rating = 5, Message = "", SeasonId = 5405, UserId = "5879560D-65C5-4699-9449-86CC57EF3111", IsAnimeInfoActive = true, IsSeasonActive = true },
             };
         }
 
@@ -215,6 +220,8 @@ namespace AnimeBrowser.UnitTests.Write.SeasonRatingTests
             var beforeCount = allSeasonRatings.Count;
             var seasonRating = requestModel.ToSeasonRating();
             seasonRating.Id = 12;
+            seasonRating.IsAnimeInfoActive = true;
+            seasonRating.IsSeasonActive = true;
             var responseModel = seasonRating.ToCreationResponseModel();
             var seasonRatingCreationHandler = sp.GetService<ISeasonRatingCreation>();
             var createdSeasonRatingResponseModel = await seasonRatingCreationHandler!.CreateSeasonRating(requestModel);
--- test/AnimeBrowser.UnitTests/Write/SeasonRatingTests/SeasonRatingEditingTests.cs
+++ test/AnimeBrowser.UnitTests/Write/SeasonRatingTests/SeasonRatingEditingTests.cs
@@ -72,8 +72,8 @@ namespace AnimeBrowser.UnitTests.Write.SeasonRatingTests
             };
 
             allAnimeInfos = new List<AnimeInfo> {
-                new AnimeInfo { Id = 1, Title = "JoJo's Bizarre Adventure", Description = string.Empty, IsNsfw = false },
-                new AnimeInfo { Id = 2, Title = "Kuroku no Basketball", Description = string.Empty, IsNsfw = false }
+                new AnimeInfo { Id = 1, Title = "JoJo's Bizarre Adventure", Description = string.Empty, IsNsfw = false, IsActive = true },
+                new AnimeInfo { Id = 2, Title = "Kuroku no Basketball", Description = string.Empty, IsNsfw = false, IsActive = true }
             };
 
             allSeasons = new List<Season>
@@ -82,46 +82,51 @@ namespace AnimeBrowser.UnitTests.Write.SeasonRatingTests
                     StartDate = new DateTime(2012, 1, 1, 0 ,0 ,0, DateTimeKind.Utc), EndDate = new DateTime(2012, 3, 5, 0 ,0 ,0, DateTimeKind.Utc),
                     AirStatus = (int)AirStatuses.Aired, NumberOfEpisodes = 24, AnimeInfoId = 1,
                     CoverCarousel = Encoding.UTF8.GetBytes("JoJoCarousel"), Cover = Encoding.UTF8.GetBytes("JoJoCover"),
+                    IsAnimeInfoActive = true, IsActive = true
                 },
                 new Season{ Id = 2, SeasonNumber = 1, Title = "Stardust Crusaders", Description = "In this season we know the story of old Joseph and young Jotaro Kujo's story while they trying to get into Egypt.",
                     StartDate = new DateTime(2014, 3, 1, 0 ,0 ,0, DateTimeKind.Utc), EndDate = new DateTime(2014, 7, 10, 0 ,0 ,0, DateTimeKind.Utc),
                     AirStatus = (int)AirStatuses.Aired, NumberOfEpisodes = 24, AnimeInfoId = 1,
                     CoverCarousel = Encoding.UTF8.GetBytes("JoJoCarousel"), Cover = Encoding.UTF8.GetBytes("JoJoCover"),
+                    IsAnimeInfoActive = true, IsActive = true
                 },
                 new Season{ Id = 5401, SeasonNumber = 1, Title = "The Pillarmen's revenge", Description = "In this season the pillarmen are taking revenge for their death.",
                     StartDate = new DateTime(2014, 3, 1, 0 ,0 ,0, DateTimeKind.Utc), EndDate = new DateTime(2014, 7, 10, 0 ,0 ,0, DateTimeKind.Utc),
                     AirStatus = (int)AirStatuses.Aired, NumberOfEpisodes = 24, AnimeInfoId = 2,
                     CoverCarousel = Encoding.UTF8.GetBytes("JoJoCarousel"), Cover = Encoding.UTF8.GetBytes("JoJoCover"),
+                    IsAnimeInfoActive = true, IsActive = true
                 },
                 new Season{ Id = 5405, SeasonNumber = 1, Title = "Life is basketball", Description = "We know the MC, who wants to get his revenge for kicking her out of the basketball team by making a new team.",
                     StartDate = today.AddYears(-10).AddMonths(-3), EndDate = today.AddYears(-9),
                     AirStatus = (int)AirStatuses.Aired, NumberOfEpisodes = 24, AnimeInfoId = 2,
                     CoverCarousel = Encoding.UTF8.GetBytes("Basketball Carousel"), Cover = Encoding.UTF8.GetBytes("Basketball Cover"),
+                    IsAnimeInfoActive = true, IsActive = true
                 },
                   new Season{ Id = 6001, SeasonNumber = 1, Title = "Monochrome", Description = "Mc sees everything in monochrome. Due to his illness, demons attack him.",
                     StartDate = null, EndDate = null,
                     AirStatus = (int)AirStatuses.NotAired, NumberOfEpisodes = 10, AnimeInfoId = 2,
                     CoverCarousel = Encoding.UTF8.GetBytes("Basketball Carousel"), Cover = Encoding.UTF8.GetBytes("Basketball Cover"),
+                    IsAnimeInfoActive = true, IsActive = true
                 }
             };
 
             allSeasonRatings = new List<SeasonRating>
             {
-                new SeasonRating { Id = 1, Rating = 4, Message = "", SeasonId = 1, UserId = "15A6B54C-98D0-4396-90E7-C94761DBA977" },
-                new SeasonRating { Id = 2, Rating = 3, Message = "Not bad.", SeasonId = 2, UserId = "15A6B54C-98D0-4396-90E7-C94761DBA977" },
-                new SeasonRating { Id = 3, Rating = 5, Message = "Cool anime!", SeasonId = 5401, UserId = "15A6B54C-98D0-4396-90E7-C94761DBA977" },
-                new SeasonRating { Id = 4, Rating = 1, Message = "Very very bad....", SeasonId = 5405, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35" },
-                new SeasonRating { Id = 5, Rating = 2, Message = "", SeasonId = 5401, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35" },
-                new SeasonRating { Id = 6, Rating = 3, Message = "", SeasonId = 2, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35" },
-                new SeasonRating { Id = 7, Rating = 4, Message = "", SeasonId = 1, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35" },
-                new SeasonRating { Id = 8, Rating = 2, Message = "", SeasonId = 5401, UserId = "5879560D-65C5-4699-9449-86CC57EF3111" },
-                new SeasonRating { Id = 9, Rating = 5, Message = "", SeasonId = 5405, UserId = "5879560D-65C5-4699-9449-86CC57EF3111" },
+                new SeasonRating { Id = 1, Rating = 4, Message = "", SeasonId = 1, UserId = "15A6B54C-98D0-4396-90E7-C94761DBA977", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 2, Rating = 3, Message = "Not bad.", SeasonId = 2, UserId = "15A6B54C-98D0-4396-90E7-C94761DBA977", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 3, Rating = 5, Message = "Cool anime!", SeasonId = 5401, UserId = "15A6B54C-98D0-4396-90E7-C94761DBA977", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 4, Rating = 1, Message = "Very very bad....", SeasonId = 5405, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 5, Rating = 2, Message = "", SeasonId = 5401, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 6, Rating = 3, Message = "", SeasonId = 2, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 7, Rating = 4, Message = "", SeasonId = 1, UserId = "65F041D2-7217-4EA6-9065-9C9AB6290B35", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 8, Rating = 2, Message = "", SeasonId = 5401, UserId = "5879560D-65C5-4699-9449-86CC57EF3111", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 9, Rating = 5, Message = "", SeasonId = 5405, UserId = "5879560D-65C5-4699-9449-86CC57EF3111", IsAnimeInfoActive = true, IsSeasonActive = true },
                 // For some reason, when episode/season was deleted, it was not deleted and still here
-                new SeasonRating { Id = 912, Rating = 2, Message = "", SeasonId = 912, UserId = "5879560D-65C5-4699-9449-86CC57EF3111" },
-                new SeasonRating { Id = 913, Rating = 5, Message = "", SeasonId = 913, UserId = "5879560D-65C5-4699-9449-86CC57EF3111" },
+                new SeasonRating { Id = 912, Rating = 2, Message = "", SeasonId = 912, UserId = "5879560D-65C5-4699-9449-86CC57EF3111", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 913, Rating = 5, Message = "", SeasonId = 913, UserId = "5879560D-65C5-4699-9449-86CC57EF3111", IsAnimeInfoActive = true, IsSeasonActive = true },
                 // For some reason, when user was deleted, it was not deleted and still here
-                new SeasonRating { Id = 916, Rating = 2, Message = "", SeasonId = 2, UserId = "CF596B99-A0CC-463B-8090-4D156B40D433" },
-                new SeasonRating { Id = 917, Rating = 5, Message = "", SeasonId = 5401, UserId = "CF596B99-A0CC-463B-8090-4D156B40D433" }
+                new SeasonRating { Id = 916, Rating = 2, Message = "", SeasonId = 2, UserId = "CF596B99-A0CC-463B-8090-4D156B40D433", IsAnimeInfoActive = true, IsSeasonActive = true },
+                new SeasonRating { Id = 917, Rating = 5, Message = "", SeasonId = 5401, UserId = "CF596B99-A0CC-463B-8090-4D156B40D433", IsAnimeInfoActive = true, IsSeasonActive = true }
             };
         }
 
@@ -239,6 +244,8 @@ namespace AnimeBrowser.UnitTests.Write.SeasonRatingTests
             });
 
             var seasonRating = requestModel.ToSeasonRating();
+            seasonRating.IsAnimeInfoActive = true;
+            seasonRating.IsSeasonActive = true;
             var responseModel = seasonRating.ToEditingResponseModel();
             var seasonRatingEditingHandler = sp.GetService<ISeasonRatingEditing>();
             var updatedSeasonRatingResponseModel = await seasonRatingEditingHandler!.EditSeasonRating(id, requestModel);
